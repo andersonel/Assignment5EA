2b53b1af048726dd7d5a85dcfeaf5aa241cbacc4
timothy <timothy@DESKTOP-T17NQQD>
2022-12-01 13:59:39 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAssignment5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstMembers = New System.Windows.Forms.ListBox()
+        Me.lblMembers = New System.Windows.Forms.Label()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.txtCount = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddMemberToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteMemberToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SortToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AscendToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DescendToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstMembers
+        '
+        Me.lstMembers.FormattingEnabled = True
+        Me.lstMembers.Location = New System.Drawing.Point(95, 98)
+        Me.lstMembers.Name = "lstMembers"
+        Me.lstMembers.Size = New System.Drawing.Size(578, 264)
+        Me.lstMembers.TabIndex = 0
+        '
+        'lblMembers
+        '
+        Me.lblMembers.AutoSize = True
+        Me.lblMembers.Font = New System.Drawing.Font("Ravie", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMembers.Location = New System.Drawing.Point(278, 40)
+        Me.lblMembers.Name = "lblMembers"
+        Me.lblMembers.Size = New System.Drawing.Size(205, 39)
+        Me.lblMembers.TabIndex = 1
+        Me.lblMembers.Text = "Members"
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.Location = New System.Drawing.Point(318, 389)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(53, 18)
+        Me.lblCount.TabIndex = 2
+        Me.lblCount.Text = "Count"
+        '
+        'txtCount
+        '
+        Me.txtCount.BackColor = System.Drawing.Color.White
+        Me.txtCount.Enabled = False
+        Me.txtCount.Location = New System.Drawing.Point(391, 387)
+        Me.txtCount.Name = "txtCount"
+        Me.txtCount.ReadOnly = True
+        Me.txtCount.Size = New System.Drawing.Size(62, 20)
+        Me.txtCount.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.SortToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.E), System.Windows.Forms.Keys)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateFileToolStripMenuItem, Me.AddMemberToolStripMenuItem, Me.DeleteMemberToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'CreateFileToolStripMenuItem
+        '
+        Me.CreateFileToolStripMenuItem.Name = "CreateFileToolStripMenuItem"
+        Me.CreateFileToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CreateFileToolStripMenuItem.Size = New System.Drawing.Size(197, 22)
+        Me.CreateFileToolStripMenuItem.Text = "Create File"
+        '
+        'AddMemberToolStripMenuItem
+        '
+        Me.AddMemberToolStripMenuItem.Name = "AddMemberToolStripMenuItem"
+        Me.AddMemberToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.AddMemberToolStripMenuItem.Size = New System.Drawing.Size(197, 22)
+        Me.AddMemberToolStripMenuItem.Text = "Add Member"
+        '
+        'DeleteMemberToolStripMenuItem
+        '
+        Me.DeleteMemberToolStripMenuItem.Name = "DeleteMemberToolStripMenuItem"
+        Me.DeleteMemberToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
+        Me.DeleteMemberToolStripMenuItem.Size = New System.Drawing.Size(197, 22)
+        Me.DeleteMemberToolStripMenuItem.Text = "Delete Member"
+        '
+        'SortToolStripMenuItem
+        '
+        Me.SortToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AscendToolStripMenuItem, Me.DescendToolStripMenuItem})
+        Me.SortToolStripMenuItem.Name = "SortToolStripMenuItem"
+        Me.SortToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.SortToolStripMenuItem.Text = "Sort"
+        '
+        'AscendToolStripMenuItem
+        '
+        Me.AscendToolStripMenuItem.Name = "AscendToolStripMenuItem"
+        Me.AscendToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.AscendToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.AscendToolStripMenuItem.Text = "Ascend"
+        '
+        'DescendToolStripMenuItem
+        '
+        Me.DescendToolStripMenuItem.Name = "DescendToolStripMenuItem"
+        Me.DescendToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Alt Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
+        Me.DescendToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
+        Me.DescendToolStripMenuItem.Text = "Descend"
+        '
+        'frmAssignment5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MintCream
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtCount)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.lblMembers)
+        Me.Controls.Add(Me.lstMembers)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmAssignment5"
+        Me.Text = "Assigment 5"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstMembers As ListBox
+    Friend WithEvents lblMembers As Label
+    Friend WithEvents lblCount As Label
+    Friend WithEvents txtCount As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SortToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddMemberToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteMemberToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AscendToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DescendToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,119 @@
+﻿'Elizabeth Anderson
+'Sequential Access and Files Programming Assignment
+'This program aloows member to be added to 
+'11/28/2022
+
+Imports System.Runtime.Remoting.Messaging
+
+Public Class frmAssignment5
+    Dim strMembersfile As String
+    Dim Count As Integer
+    Private Sub frmAssignment5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' COMMENT
+        strMembersfile = "Youth_Members-2.txt"
+        lstMembers.DataSource = IO.File.ReadAllLines(strMembersfile)
+        lstMembers.SelectedItem = Nothing
+        Count = lstMembers.Items.Count()
+        txtCount.Text = Count
+    End Sub
+    Private Sub AscendToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AscendToolStripMenuItem.Click
+        ' This looks fun. This is LINQ, I thinq. Did this get generated or did you write it yourself?
+        Dim Ascendquery = From name In IO.File.ReadAllLines(strMembersfile)
+                          Order By name Ascending
+                          Select name
+        lstMembers.DataSource = Ascendquery.ToList
+    End Sub
+    Private Sub DescendToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DescendToolStripMenuItem.Click
+        Dim Descendquery = From name In IO.File.ReadAllLines(strMembersfile)
+                           Order By name Descending
+                           Select name
+        lstMembers.DataSource = Descendquery.ToList
+    End Sub
+    Private Sub AddMemberToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddMemberToolStripMenuItem.Click
+        ' I am commenting out your original code, rather than replacing it
+        'Dim Newmember As String = InputBox("Please enter New member's Full name")
+        ' If Newmember = " " Then
+        ' MessageBox.Show("Please enter a REAL name")
+        ' 'HOW
+        ' ElseIf Newmember = "" Then
+        ' MessageBox.Show("Please enter a name")
+        ' Return 'input box leaxes completley, needs to return to input box
+        ' 'option to have message say try again ? 
+        ' End If
+
+        ' Use a function to go get a member name. It will not come back until it gets a good one
+        Dim Newmember As String = input_member()
+
+        '        Dim SW As IO.StreamWriter
+        '        SW = IO.File.AppendText(strMembersfile)
+        ' probably should do it in one line instead
+        ' I think Using/End Using is a good idea
+        ' your instructor will like it if they do Python because that's how you're supposed to do it there
+        'https://learn.microsoft.com/en-us/dotnet/api/system.io.file.appendtext?view=net-6.0#examples
+        Dim SW As IO.StreamWriter = IO.File.AppendText(strMembersfile)
+
+        SW.WriteLine(Newmember)
+        SW.Close()
+        lstMembers.DataSource = IO.File.ReadAllLines(strMembersfile)
+        lstMembers.SelectedItem = Nothing
+        Count = lstMembers.Items.Count()
+        txtCount.Text = Count
+    End Sub
+
+    Private Function input_member() As String
+        ' completely untested, but probably super close to working.
+        Dim new_member As String = ""
+        Dim valid_member As Boolean = False
+
+        ' This while block will run over and over, while that condition (Not valid_member) is true.
+        While Not valid_member
+            new_member = InputBox("Please enter New member's Full name")
+            If new_member = " " Then
+                MessageBox.Show("Please enter a name")
+            ElseIf new_member = String.Empty Then
+                MessageBox.Show("Please enter a REAL name")
+            ElseIf new_member = "(you can add other checks if you want, with more ElseIfs)" Then
+                MessageBox.Show("Super Specific Error Message")
+            Else
+                ' we have checked all ways of the input being bad,
+                ' all checks have passed so we should exit the while loop.
+                valid_member = True
+            End If
+        End While
+
+        Return new_member
+    End Function
+
+    Private Sub DeleteMemberToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteMemberToolStripMenuItem.Click
+        'Error Check something is selected 
+        If lstMembers.SelectedIndex = -1 Then
+            ' Maybe you could enable/disable the delete item itself, maybe in lstMembers_SelectedIndexChanged?
+            MessageBox.Show("No member selected to delete")
+            Return
+        End If
+        Dim deleteMember As String = lstMembers.SelectedItem
+        Dim deleteQuery = From reading In IO.File.ReadAllLines(strMembersfile)
+                          Where reading <> deleteMember
+                          Select reading
+        IO.File.WriteAllLines(strMembersfile, deleteQuery)
+        lstMembers.DataSource = IO.File.ReadAllLines(strMembersfile)
+        lstMembers.SelectedItem = Nothing
+        Count = lstMembers.Items.Count()
+        txtCount.Text = Count
+    End Sub
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        'Quits Program
+        Close()
+    End Sub
+
+    Private Sub lstMembers_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstMembers.SelectedIndexChanged
+        '        If lstMembers.SelectedItems.Count == 0 Then
+        '        DeleteMemberToolStripMenuItem.Enabled = False
+        '        Else
+        '        DeleteMemberToolStripMenuItem.Enabled = True
+        '        End If
+
+        ' a quicker way to do the same thing: (brackets not really needed, but it might make it clearer for you
+        DeleteMemberToolStripMenuItem.Enabled = (lstMembers.SelectedItems.Count <> 0)
+    End Sub
+End Class
